320b47aedc18209491e5bf5de121005b7a391c02
Thien52213 <nguyenthanhthien201195@gmail.com>
2024-05-30 16:58:40 +0700

    Commit code lan dau

---
+++ src/anhtester/com/javabasic/Kieudulieu/KieuDuLieu1.java
@@ -0,0 +1,23 @@
+package anhtester.com.javabasic.Kieudulieu;
+
+public class KieuDuLieu1 {
+    //static int numberInt = 1042479;
+     int numberInt = 10424799;
+
+     static String doanVan = "Mỗi đoạn văn thường bắt đầu bằng một câu chủ đề hoặc một ý chính, từ đó phát triển và mở rộng ý kiến, thông tin hoặc quan điểm của tác giả. Các câu trong đoạn văn liên kết với nhau thông qua những từ nối, ví dụ như ";
+
+     boolean checkValue = true;
+
+    public static void main(String[] args) {
+        //System.out.println(numberInt);
+
+        System.out.println(doanVan);
+
+//        KieuDuLieu1 kieuDuLieu1 = new KieuDuLieu1();
+//        System.out.println(kieuDuLieu1.numberInt);
+
+        System.out.println(Bank.BANKNAME);
+        System.out.println(Bank.VAT);
+        System.out.println(Bank.INTEREST_RATE);
+    }
+}
---
+++ src/anhtester/com/javabasic/phuongthuc/PhuongThucBank.java
@@ -0,0 +1,14 @@
+package anhtester.com.javabasic.phuongthuc;
+
+import anhtester.com.javabasic.Kieudulieu.Bank;
+
+public class PhuongThucBank {
+
+    public static void main(String[] args) {
+        Bank.getBankInfo();
+        double tongTien = (Bank.getInterestRate() * 100000000) / 100;
+        System.out.println("Tổng tiền lãi: " + tongTien);
+
+    }
+
+}
